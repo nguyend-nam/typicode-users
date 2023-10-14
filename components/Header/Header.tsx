29eaf1c6ccf8f8f13fa62283b5164c3f6428dafc
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2023-10-15 02:18:53 +0700

    feat: implement counter page (#2)

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,19 @@
+import { useRouter } from "next/router";
 import { Avatar } from "../Avatar/Avatar";
 import { IconHelp, IconNotification } from "../icons";
 
 export const Header = () => {
+  const { push } = useRouter();
   return (
     <header className="w-screen bg-blue--dark px-7 py-5 fixed top-0 z-40">
       <div className="section-container text-white flex justify-end items-center">
         <div className="pr-4 border-r-[1.5px] flex items-center">
-          <button>
+          <button onClick={() => push("/")}>
             <IconNotification />
           </button>
         </div>
         <div className="px-4 border-r-[1.5px] flex items-center">
-          <button>
+          <button onClick={() => push("/counter")}>
             <IconHelp />
           </button>
         </div>
